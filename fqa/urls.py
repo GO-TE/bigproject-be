7151968cc5f5c33636676a792abf69b112ea7c30
siisjo <122655426+siisjo@users.noreply.github.com>
2024-07-16 13:22:49 +0900

    fix(fqa) : urls 오타 수정

--- fqa/urls.py
+++ fqa/urls.py
@@ -5,6 +5,6 @@ from .views import (
 )
 
 urlpatterns = [
-    path('faqs/', FAQListView.as_view(), name='faq-list'),
-    path('faqs/category/<str:category>/', FAQByCategoryListView.as_view(), name='faq-by-category'),
+    path('fqas/', FAQListView.as_view(), name='faq-list'),
+    path('fqas/category/<str:category>/', FAQByCategoryListView.as_view(), name='faq-by-category'),
 ]
