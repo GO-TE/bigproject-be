ce7f10f210d046c678e620d191905f32b5c5710c
GO-TE <alsrudxo9810@naver.com>
2024-07-18 10:15:27 +0900

    test(fqa): 법 데이터 조회 테스트 코드 작성

--- fqa/tests.py
+++ fqa/tests.py
@@ -1,9 +1,16 @@
 # tests.py
+from datetime import date
+
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
-from .models import FAQ
+from .models import (
+    FAQ,
+    Law,
+    Rule,
+    Glossary
+)
 from community.models import Category
 
 
@@ -23,3 +30,33 @@ class FAQListViewTest(TestCase):
         self.assertEqual(len(response.data), 2)
         self.assertEqual(response.data[0]['question'], self.faq1.question)
         self.assertEqual(response.data[1]['question'], self.faq2.question)
+
+
+class LawListViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('law')
+
+        for i in range(31):
+            Law.objects.create(
+                law=f"Test law{i}",
+                ministry="고용노동부",
+                code="10121",
+                content=f"사장님 돈 좀 많이 주세요 제발요{i}",
+                date=date(2023, 1, 1)
+            )
+
+    def test_list_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 20)
+
+    def test_search_laws_by_title(self):
+        response = self.client.get(self.url, {'title': "Test law1"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(any("Test law1" in law['law'] for law in response.data['results']))
+
+    def test_search_laws_by_content(self):
+        response = self.client.get(self.url, {'content': "사장님"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(any("사장님" in law['content'] for law in response.data['results']))
