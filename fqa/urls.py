64098ca08de3b083f8787b2d82fa9633b0c4584b
GO-TE <alsrudxo9810@naver.com>
2024-07-18 10:16:34 +0900

    feat(fqa): 법 데이터 조회 기능 구현

--- fqa/urls.py
+++ fqa/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     FAQListView,
-    FAQByCategoryListView
+    FAQByCategoryListView,
+    LawListView,
 )
 
 urlpatterns = [
     path('fqas/', FAQListView.as_view(), name='faq-list'),
     path('fqas/category/<str:category>/', FAQByCategoryListView.as_view(), name='faq-by-category'),
+    path('law', LawListView().as_view(), name='law')
 ]
--- fqa/views.py
+++ fqa/views.py
@@ -1,9 +1,21 @@
+from rest_framework import filters
 from rest_framework.generics import ListAPIView
-
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from .models import FAQ
-from .serializers import FAQSerializer
+from .models import (
+    FAQ,
+    Law,
+    Rule,
+    Glossary
+)
+from .serializers import (
+    FAQSerializer,
+    LawSerializer,
+    RuleSerializer,
+    GlossarySerializer
+)
+from .pagination import FQAPagination
 
 
 class FAQListView(ListAPIView):
@@ -19,3 +31,26 @@ class FAQByCategoryListView(ListAPIView):
     def get_queryset(self):
         category = self.kwargs['category']
         return FAQ.objects.filter(category=category)
+
+
+class LawListView(ListAPIView):
+    queryset = Law.objects.all()
+    serializer_class = LawSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+    pagination_class = FQAPagination
+    permission_classes = [AllowAny]
+
+
+    def get_queryset(self):
+        queryset = Law.objects.all().order_by('-law')
+        title_query = self.request.query_params.get('title', None)
+        content_query = self.request.query_params.get('content', None)
+
+        if title_query:
+            queryset = queryset.filter(law__icontains=title_query)
+        if content_query:
+            queryset = queryset.filter(content__icontains=content_query)
+
+        return queryset
+
