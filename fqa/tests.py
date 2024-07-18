18c88497fc07006de290213188cdd9da096425ec
GO-TE <alsrudxo9810@naver.com>
2024-07-18 10:43:35 +0900

    test(fqa): 행정규칙, 법령용어 테스트 코드 작성

--- fqa/tests.py
+++ fqa/tests.py
@@ -60,3 +60,62 @@ class LawListViewTest(TestCase):
         response = self.client.get(self.url, {'content': "사장님"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(any("사장님" in law['content'] for law in response.data['results']))
+
+
+class RuleListViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('rule')
+
+        for i in range(31):
+            Rule.objects.create(
+                rule=f"Test rule{i}",
+                kind="무슨무슨 법",
+                code="10121",
+                content=f"사장님 돈 좀 많이 주세요 제발요{i}",
+                effective=date(2023, 1, 1),
+                created=date(2023, 1, 1),
+            )
+
+    def test_list_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 20)
+
+    def test_search_laws_by_title(self):
+        response = self.client.get(self.url, {'title': "Test rule1"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(any("Test rule1" in rule['rule'] for rule in response.data['results']))
+
+    def test_search_laws_by_content(self):
+        response = self.client.get(self.url, {'content': "사장님"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(any("사장님" in rule['content'] for rule in response.data['results']))
+
+
+class GlossaryListViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('glossary')
+
+        for i in range(31):
+            Glossary.objects.create(
+                terminology=f"있어보이는말{i}",
+                code=f'1232{i}',
+                content=f"좀 어려워서 안써도 될듯{i}"
+            )
+
+    def test_list_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 20)
+
+    def test_search_laws_by_title(self):
+        response = self.client.get(self.url, {'title': "있어보이는말1"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(any("있어보이는말1" in glossary['terminology'] for glossary in response.data['results']))
+
+    def test_search_laws_by_content(self):
+        response = self.client.get(self.url, {'content': "좀 어려워서"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(any("어려워서" in glossary['content'] for glossary in response.data['results']))
