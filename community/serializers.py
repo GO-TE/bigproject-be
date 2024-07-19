96d7a0e62c17ba33104f2ded3c425ee2b268e1ff
GO-TE <alsrudxo9810@naver.com>
2024-07-19 10:28:46 +0900

    fix(community): 유저 닉네임 반환 수정

--- community/serializers.py
+++ community/serializers.py
@@ -1,24 +1,34 @@
 from rest_framework import serializers
 from .models import Article, Comment
+from account.models import User
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    nickname = serializers.SerializerMethodField()
+
     class Meta:
         model = Comment
-        fields = ['id', 'article', 'user', 'message', 'created_at', 'updated_at']
+        fields = ['id', 'article', 'nickname', 'message', 'created_at', 'updated_at']
         read_only_fields = ['user', 'created_at', 'updated_at']
+
+    def get_user_nickname(self, obj):
+        return obj.user.nickname
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     comments = serializers.SerializerMethodField()
+    nickname = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
-        fields = ['id', 'user', 'image', 'category', 'title', 'content', 'created_at', 'updated_at', 'view', 'is_active', 'comments']
+        fields = ['id', 'nickname', 'image', 'category', 'title', 'content', 'created_at', 'updated_at', 'view', 'is_active', 'comments']
 
     def get_comments(self, obj):
         comments = obj.comment_set.all()
         return CommentSerializer(comments, many=True).data
+
+    def get_uesr_nickname(self, obj):
+        return obj.user.nickname
 
 
 class ArticleListSerializer(serializers.ModelSerializer):
