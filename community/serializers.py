cb7bcb50e829324e7da0c76c6524b323fa893b6e
GO-TE <alsrudxo9810@naver.com>
2024-07-19 15:35:14 +0900

    fix(community): 글 조회시 작성자 여부 반환

--- community/serializers.py
+++ community/serializers.py
@@ -14,21 +14,25 @@ class CommentSerializer(serializers.ModelSerializer):
 class ArticleSerializer(serializers.ModelSerializer):
     comments = serializers.SerializerMethodField()
     user_nickname = serializers.CharField(source='user.nickname', read_only=True)
+    is_author = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
         fields = ['id', 'user_nickname', 'image', 'category', 'title', 'content', 'created_at', 'updated_at', 'view',
-                  'is_active', 'comments']
+                  'is_active', 'comments', 'is_author']
 
     def get_comments(self, obj):
         comments = obj.comment_set.all()
         return CommentSerializer(comments, many=True).data
 
+    def get_is_author(self, obj):  # is_author 필드를 계산하기 위한 메서드
+        request = self.context.get('request')
+        return obj.user == request.user if request else False
+
 
 class ArticleListSerializer(serializers.ModelSerializer):
     user_nickname = serializers.CharField(source='user.nickname', read_only=True)
     comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
-
 
     class Meta:
         model = Article
